556d296606fa62d9625038da29d0e0da18c5c7ce
Interchained <evansmark.work@gmail.com>
2020-11-11 03:11:19 -0500

    sorting

--- js/history.js
+++ js/history.js
@@ -16,24 +16,35 @@ $(document).ready(function() {
     $('.sorting_asc').attr('id', 'sorting_asc_coins');
     $('.sorting_desc').attr('id', 'sorting_desc_coins');
     $('.input-sm').attr('id', 'sorting_coins');
-    console.log($('.row_GLDX even'))
-    var coin_selected = $(".btn-selected").attr("id");
-        switch(coin_selected){
-        case 'etnx-coin':
-                $("#sorting_asc_coins").click();
-            return $('.input-sm').val("ETNX");
-        case 'etnxp-coin':
-                $("#sorting_desc_coins").click();
-            return $('.input-sm').val("ETNXP");
-        case 'etnxc-coin':
-            return $('.input-sm').val("ETNXC"); 
-        case 'ltnx-coin':
-            return $('.input-sm').val("LTNX"); 
-        case 'gldx-coin':
-            return $('.input-sm').val("GLDX"); 
-        case 'crfi-coin':
-            return $('.input-sm').val("CRFI"); 
-        default:
-            break;
-        }     
+    console.log($('.row_GLDX even'));
+    console.log($('.row_GLDX even')[0]);
+    function etnxSort() {
+        //$("#sorting_asc_coins").click();
+        $(".sorting_asc").click();
+  document.getElementsByClassName("input-sm").innerHTML = "ETNX";
+}
+    function etnxpSort() {
+        $("#sorting_desc_coins").click();
+        //$(".sorting_desc").click();
+  document.getElementsByClassName("input-sm").innerHTML = "ETNXP";
+}
+    function etnxcSort() {
+  document.getElementsByClassName("input-sm").innerHTML = "ETNXC";
+}
+    function ltnxSort() {
+  document.getElementsByClassName("input-sm").innerHTML = "LTNX";
+}
+    function gldxSort() {
+  document.getElementsByClassName("input-sm").innerHTML = "GLDX";
+}
+    function crfiSort() {
+  document.getElementsByClassName("input-sm").innerHTML = "CRFI";
+}
+    document.getElementById("etnx-coin").addEventListener("click", etnxSort);
+    document.getElementById("etnxp-coin").addEventListener("click", etnxpSort);
+    document.getElementById("etnxc-coin").addEventListener("click", etnxcSort);
+    document.getElementById("ltnx-coin").addEventListener("click", ltnxSort);
+    document.getElementById("gldx-coin").addEventListener("click", gldxSort);
+    document.getElementById("crfi-coin").addEventListener("click", crfiSort);
+    
 });
