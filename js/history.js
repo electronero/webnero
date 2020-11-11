225c4ae6bd68a7fd14a6bc2257ffb383382594fe
Interchained <evansmark.work@gmail.com>
2020-11-11 02:49:50 -0500

    should select coins, select data

--- js/history.js
+++ js/history.js
@@ -10,4 +10,26 @@ $(document).ready(function() {
         responsive: true,
         "order": [[ 3, 'desc' ]]
     });
+    var allInputs = $(":input");
+    //allInputs.attr('type');
+    //$("#").click();
+    var coin_selected = $(".btn-selected").attr("id");
+	    switch(coin_selected){
+		case 'etnx-coin':
+                $(".sorting_asc").click();
+		    return allInputs.val("ETNX");
+		case 'etnxp-coin':
+		    return allInputs.val("ETNXP");
+		case 'etnxc-coin':
+		    return allInputs.val("ETNXC"); 
+		case 'ltnx-coin':
+            return allInputs.val("LTNX"); 
+        case 'gldx-coin':
+		    return allInputs.val("GLDX"); 
+        case 'crfi-coin':
+		    return allInputs.val("CRFI"); 
+        default:
+            break;
+	    }
+    }     
 });
