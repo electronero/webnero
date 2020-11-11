b528ae9dd1b72c14a0dacfb1ea6ca20ece49dac7
Interchained <evansmark.work@gmail.com>
2020-11-11 02:56:34 -0500

    sorting

--- js/history.js
+++ js/history.js
@@ -10,15 +10,19 @@ $(document).ready(function() {
         responsive: true,
         "order": [[ 3, 'desc' ]]
     });
-    var allInputs = $(":input");
+    var allInputs = $("#sorting_coins");
     //allInputs.attr('type');
     //$("#").click();
+    $('.sorting_asc').attr('id', 'sorting_asc_coins');
+    $('.sorting_desc').attr('id', 'sorting_desc_coins');
+    $('.input-sm').attr('id', 'sorting_coins');
     var coin_selected = $(".btn-selected").attr("id");
         switch(coin_selected){
         case 'etnx-coin':
-                $(".sorting_asc").click();
+                $("#sorting_asc_coins").click();
             return allInputs.val("ETNX");
         case 'etnxp-coin':
+                $("#sorting_desc_coins").click();
             return allInputs.val("ETNXP");
         case 'etnxc-coin':
             return allInputs.val("ETNXC"); 
